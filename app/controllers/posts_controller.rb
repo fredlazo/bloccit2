1e5e2085183b1d4707dc4608b3c2d8642e5b75d7
Frederick Lazo <fredlazo@yahoo.com>
2016-06-22 10:05:08 -0700

    Checkpoint 32 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,33 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Your post was saved successfully."
+      redirect_to @post
+    else
+      flash.now[:alert] = "Your post was not saved. Try again."
+      render :new
+    end
   end
 
   def edit
   end
+
+  private
+    def post_params
+      params.require(post).permit(title,body)
+    end
+
 end
