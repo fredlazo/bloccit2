7535fc86fccebc27fa591ba28d26db63118cd269
Frederick Lazo <fredlazo@yahoo.com>
2016-06-24 14:08:18 -0700

    Assignment 35 completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,6 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  validates :description, length: { minimum: 20 }, presence: true
+  validates :name, length: { minimum: 5}, presence: true
 end
