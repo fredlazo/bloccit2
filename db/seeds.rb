debedc39b86f1c6b182ea71ae1a8edec669e1b4d
Frederick Lazo <fredlazo@yahoo.com>
2016-06-25 20:37:27 -0700

    Checkpoint 40 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,10 +42,19 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'fredlazo@yahoo.com', # replace this with your personal email
-  password: 'giants2010'
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
 )
 
 puts "Seed finished"
