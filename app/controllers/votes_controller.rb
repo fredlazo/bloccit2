f30cd1410c8616aca8ae5234e4c38276deee91a0
Frederick Lazo <fredlazo@yahoo.com>
2016-06-28 15:27:23 -0700

    Checkpoint 43 completed

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -12,16 +12,16 @@ class VotesController < ApplicationController
   end
 
   private
-  
-  def update_vote(new_value)
-    @post = Post.find(params[:post_id])
-    @vote = @post.votes.where(user_id: current_user.id).first
 
-    if @vote
-      @vote.update_attribute(:value, new_value)
-    else
-      @vote = current_user.votes.create(value: new_value, post: @post)
+    def update_vote(new_value)
+      @post = Post.find(params[:post_id])
+      @vote = @post.votes.where(user_id: current_user.id).first
+
+      if @vote
+        @vote.update_attribute(:value, new_value)
+      else
+        @vote = current_user.votes.create(value: new_value, post: @post)
+      end
     end
-  end
 
 end
